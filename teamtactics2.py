3b6beb6ce494775f9c1acc0772e8264f865670ad
robbyb <robert@enterprise>
2020-02-21 23:51:03 +0100

    InnoSetup added, pushed to version 0.99

--- teamtactics2.py
+++ teamtactics2.py
@@ -29,7 +29,7 @@ __email__ =  "rbausdorf@gmail.com"
 __license__ = "GPLv3"
 #__maintainer__ = "developer"
 __status__ = "Beta"
-__version__ = "0.98"
+__version__ = "0.99"
 
 import sys
 import configparser
